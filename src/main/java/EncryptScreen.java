ab2d40951f55b6158a148e0553cc1dd3cd0da1e5
Nandan Tumu <t.nandan2008@gmail.com>
2016-02-25 01:09:01 -0500

    HomeScreen and Encrypt screen complete

--- src/main/java/EncryptScreen.java
+++ src/main/java/EncryptScreen.java
@@ -1,5 +1,21 @@
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * Created by Nandan on 2/10/2016.
  */
 public class EncryptScreen {
+    private JPanel panel1;
+    private JButton button1;
+
+    public static void main() {
+        JFrame frame = new JFrame("EncryptScreen");
+        frame.setContentPane(new EncryptScreen().panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    private JButton encryptButton;
+
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,4 +2,9 @@
  * Created by Nandan on 2/8/2016.
  */
 public class Main {
+    public static void main(String [] args){
+        //This class shall call all other classes.
+        HomeScreen hs = new HomeScreen();
+        HomeScreen.main();
+    }
 }
