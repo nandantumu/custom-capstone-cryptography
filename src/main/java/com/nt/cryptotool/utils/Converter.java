4587c914e0f6422c582719d45e7114b964efa88e
Nandan T <t.nandan2008@gmail.com>
2016-03-18 13:00:43 -0400

    Added conversion method.

--- src/main/java/com/nt/cryptotool/utils/Converter.java
+++ src/main/java/com/nt/cryptotool/utils/Converter.java
@@ -1,13 +1,23 @@
 package com.nt.cryptotool.utils;
 
 import java.math.BigInteger;
+import java.util.BitSet;
+
+import static java.util.BitSet.valueOf;
 
 /**
  * Created by Nandan on 3/11/2016.
  */
 public class Converter {
-    Converter(){
+    public Converter(){
 
+    }
+
+    public BitSet byteToBits(byte[] bytearray){
+        BitSet returnValue = new BitSet();
+        //noinspection Since15
+        returnValue = BitSet.valueOf(bytearray);
+        return returnValue;
     }
 
     public BigInteger stringToBigInt(String input){
