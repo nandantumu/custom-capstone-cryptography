1e639e60bd5c4b6336f9875caf4aad5061441eac
Nandan Tumu <t.nandan2008@gmail.com>
2016-03-22 13:39:28 -0400

    Edited Controller to function as desired

--- src/main/java/com/nt/cryptotool/GUIFiles/HomeScreenController.java
+++ src/main/java/com/nt/cryptotool/GUIFiles/HomeScreenController.java
@@ -1,33 +1,57 @@
 package com.nt.cryptotool.GUIFiles;
 
-import com.nt.cryptotool.MainApp;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 
-public class HomeScreenController extends BorderPane
-{
+public class HomeScreenController {
 
+    @FXML
+    private ResourceBundle resources;
 
-    @FXML private Button keyGenButton;
-    @FXML private Button decryptButton;
-    @FXML private Button encryptButton;
-    private MainApp main;
+    @FXML
+    private URL location;
 
-    HomeScreenController(MainApp main){
-        this.main = main;
-    }
+    @FXML
+    private BorderPane HomeScreenRootPane;
 
-    HomeScreenController(){
+    @FXML
+    private Button KeyGenButton;
 
-    }
+    @FXML
+    private Button EncryptButton;
 
-    public void onClickEncryptButton(){
-        try {
-            main.changeView("EncryptScreen.fxml");
-        }
-        catch (Exception e){
+    @FXML
+    private Button DecryptButton;
 
-        }
+    @FXML
+    void initialize() {
+        assert HomeScreenRootPane != null : "fx:id=\"HomeScreenRootPane\" was not injected: check your FXML file 'HomeScreen.fxml'.";
+        assert KeyGenButton != null : "fx:id=\"KeyGenButton\" was not injected: check your FXML file 'HomeScreen.fxml'.";
+        assert EncryptButton != null : "fx:id=\"EncryptButton\" was not injected: check your FXML file 'HomeScreen.fxml'.";
+        assert DecryptButton != null : "fx:id=\"DecryptButton\" was not injected: check your FXML file 'HomeScreen.fxml'.";
+        EncryptButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("EncryptButton click handled");
+            }
+        });
+        DecryptButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("DecryptButton click handled");
+            }
+        });
+        KeyGenButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("KeyGenButton click handled");
+            }
+        });
     }
 }
