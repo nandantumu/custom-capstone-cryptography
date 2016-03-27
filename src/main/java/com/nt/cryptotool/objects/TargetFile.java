59abac4cc19805ef1d49119a9350a25918d585b6
Nandan Tumu <t.nandan2008@gmail.com>
2016-03-27 16:44:29 -0400

    Added enum to constructor,

--- src/main/java/com/nt/cryptotool/objects/TargetFile.java
+++ src/main/java/com/nt/cryptotool/objects/TargetFile.java
@@ -2,10 +2,14 @@ package com.nt.cryptotool.objects;
 
 
 import java.io.*;
+import java.util.BitSet;
 
+import com.nt.cryptotool.miscobjects.Task;
+import com.nt.cryptotool.utils.Converter;
+import org.apache.commons.io.IOExceptionWithCause;
 import org.apache.commons.io.IOUtils;
 
-/**
+/**This class represents the file to be
  * Created by Nandan on 3/21/2016.
  */
 public class TargetFile {
@@ -13,17 +17,28 @@ public class TargetFile {
     private File targetFile;
     private FileInputStream fileInputStream;
     private FileOutputStream fileOutputStream;
+    private Converter converter = new Converter();
 
-    public TargetFile(String url) throws FileNotFoundException{
-        sourceFile = new File(url);
-        targetFile = new File(url.substring(0,url.indexOf(sourceFile.getName())).concat("EncryptedFile.secure"));
+    public TargetFile(String url, Task task) throws FileNotFoundException{
+        if(task==Task.Encrypt) {
+            sourceFile = new File(url);
+            targetFile = new File(url.substring(0, url.indexOf(sourceFile.getName())).concat("EncryptedFile.secure"));
+        }
+        else if(task == Task.Decrypt){
+            sourceFile = new File(url);
+            targetFile = new File(url.substring(0, url.indexOf(sourceFile.getName())).concat("DecryptedFile.secure"));
+        }
         fileInputStream = new FileInputStream(sourceFile);
         fileOutputStream = new FileOutputStream(targetFile);
         targetFile.mkdirs();
     }
 
-    public byte[] getContents() throws IOException {
+    public byte[] getBytesContents() throws IOException {
         return IOUtils.toByteArray(fileInputStream);
+    }
+
+    public BitSet getBitsContents() throws IOException{
+        return converter.byteToBits(IOUtils.toByteArray(fileInputStream));
     }
 
     public int getFileSize(){
